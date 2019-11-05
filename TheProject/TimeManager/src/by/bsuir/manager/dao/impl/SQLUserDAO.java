30e82a1c6d0fee326fac1c5306cd7fc8f6a258df
EugeneBlizniuk <janey1102@mail.ru>
2019-11-05 15:50:00 +0300

    classes are added(DAO, Service, Controller)

---
+++ TheProject/TimeManager/src/by/bsuir/manager/dao/impl/SQLUserDAO.java
@@ -0,0 +1,16 @@
+package by.bsuir.manager.dao.impl;
+
+import by.bsuir.manager.bean.User;
+import by.bsuir.manager.dao.UserDAO;
+
+public class SQLUserDAO implements UserDAO {
+    @Override
+    public void signIn(String login, String password) {
+
+    }
+
+    @Override
+    public void signUp(User user) {
+
+    }
+}
