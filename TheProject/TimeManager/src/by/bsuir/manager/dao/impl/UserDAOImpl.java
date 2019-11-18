a3c364121e91e79de0f873200f3e911c0cef9df6
EugeneBlizniuk <janey1102@mail.ru>
2019-11-18 18:15:10 +0300

    playing with databases

--- TheProject/TimeManager/src/by/bsuir/manager/dao/impl/UserDAOImpl.java
+++ TheProject/TimeManager/src/by/bsuir/manager/dao/impl/UserDAOImpl.java
@@ -5,13 +5,78 @@ import by.bsuir.manager.dao.exception.DAOException;
 import by.bsuir.manager.entity.User;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class UserDAOImpl implements UserDAO {
     private static final String SQL_SELECT_ALL_USERS = "SELECT id, login FROM userBook";
-    private static final String SQL_ADD_A_USER = "INSERT INTO userTable (login, password) VALUES(?,?)";
+    private static final String SQL_ADD_A_USER = "INSERT INTO users(id, login, password, current_time) VALUES(?, ?, ?, ?)";
     private static final String SQL_SELECT_USER_BY_LOGIN = "SELECT login FROM userBook WHERE id=?";
+
+    {
+        try {
+            Class.forName("org.postgresql.Driver");
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection getDBConnection() {
+        Connection connection = null;
+        String url = "jdbc:postgresql://localhost:5432/java_ee_db";
+        Properties properties = new Properties();
+
+        properties.put("user", "postgres");
+        properties.put("password", "09102014Qm");
+        properties.put("autoReconnect", "true");
+        properties.put("characterEncoding", "UTF-8");
+        properties.put("useUnicode", "true");
+        properties.put("useSSL", "true");
+
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/users_db",
+                    "postgres", "09102014Qm");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    @Override
+    public boolean signUp(User user) {
+        boolean isCorrect = false;
+        Connection connection = getDBConnection();
+        if(connection == null) {
+            //log
+            return isCorrect;
+        }
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement(SQL_ADD_A_USER);
+
+            statement.setInt(1, user.getId());
+            statement.setString(2, user.getLogin());
+            statement.setString(3, user.getPassword());
+            statement.setString(4, getCurrentTimeStamp());
+            statement.executeUpdate();
+
+            isCorrect = true;
+        } catch (SQLException e) {
+            //log
+            e.printStackTrace();
+        } finally {
+            close(statement);
+            close(connection);
+        }
+
+        return isCorrect;
+    }
 
     @Override
     public List<User> findAll() {
@@ -113,10 +178,8 @@ public class UserDAOImpl implements UserDAO {
         return false;
     }
 
-    @Override
-    public boolean signUp(User user) {
-        boolean isCorrect = false;
-
-        return isCorrect;
+    private String getCurrentTimeStamp() {
+        return LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
     }
 }
